52ad397a7ac99d8a9ca63368ead8bdd5696e5a56
Dominguez <jldm-1111@outlook.com>
2023-11-11 21:15:14 -0600

    Preparando motores

--- Pruebas/motores/motores.ino
+++ Pruebas/motores/motores.ino
@@ -1,19 +1,27 @@
-#include <motores.h>
 #include <Arduino.h>
-#include <Wire.h>
 
-int motor1 = 2;
-int motor2 = 3;
-void setup()
-{
-    Serial.begin(9600);
-    Serial.println("Iniciando");
 
+
+
+//Constantes
+double Kp=0, Ki=0, Kd=0;
+
+//Variables externas
+double setpoint=0, input=0, output=0;
+
+//Variables internas
+double error=0, errorPrev=0, errorAcum=, errorDeriv=0;
+unsigned long tiempoPrev=0, tiempoActual=0;
+double tiempoTranscurrido=0;
+
+
+
+void setup(){
 
 
 }
-void loop()
-{
-
+void loop(){
 
 }
+
+
