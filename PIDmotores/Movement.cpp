9dff29209bd225943ca1c1d07f9356095ddbcda1
Dominguez <jldm-1111@outlook.com>
2023-12-11 19:43:32 -0600

    Cambios para pull

--- PIDmotores/Movement.cpp
+++ PIDmotores/Movement.cpp
@@ -3,32 +3,16 @@
 #include "Pines.h"
 //#include "Encoder.h"
 
-
 // Ajustar
 // JALAN 2/3
 constexpr double kP = 5.0; 
 constexpr double kI = 0.008;
 constexpr double kD = 0.0;
 
-/* constexpr double kP = 5.0; 
-constexpr double kI = 0.09;
-constexpr double kD = 0.07; */
-
-/* constexpr double kP = 120.0; 
-constexpr double kI = 80;
-constexpr double kD = 10.0; */
-
 PID pidStraight(kP, kI, kD);
 BNO bno;
 
-
-
-
 Movement::Movement() {
-    this-> motorFL = Motor();
-    this-> motorFR = Motor();
-    this-> motorBL = Motor();
-    this-> motorBR = Motor();
     this->motor[4];
 
     this-> next_time = millis();
@@ -68,69 +52,6 @@ void Movement::moveForward(int pwmA, int pwmB, int pwmC, int pwmD) {
 
 }
 
-void Movement::setSpeed(float targetSpeed,float orientation,BNO bno) {
-    //PID orientation
-    float errorOrientation = orientation - bno.getOrientationX();
-
-    if (errorOrientation > 300) {
-        errorOrientation = orientation - (360 + bno.getOrientationX());
-    }
-
-    if (errorOrientation < -300) {
-        errorOrientation = (bno.getOrientationX() - (360 + orientation)) * -1;
-    }
-    errorAcumuladoOrientation = errorAcumuladoOrientation + errorOrientation;
-    //float targetSpeedRight = targetSpeed - (kP * errorOrientation + kI * (errorAcumuladoOrientation) + kD * (errorOrientation - errorPrevOrientation));
-    float targetSpeedRight = - (kP * errorOrientation + kI * (errorAcumuladoOrientation) + kD * (errorOrientation - errorPrevOrientation));
-    //float targetSpeedLeft = targetSpeed + (kP * errorOrientation + kI * (errorAcumuladoOrientation) + kD * (errorOrientation - errorPrevOrientation));
-    float targetSpeedLeft = (kP * errorOrientation + kI * (errorAcumuladoOrientation) + kD * (errorOrientation - errorPrevOrientation));
-    errorPrevOrientation = errorOrientation;
-
-    // TODO: checar espacios de if mas que todo formato
-    if (targetSpeedLeft > 255)
-        targetSpeedLeft = 255;
-    else if (targetSpeedLeft < 0)
-        targetSpeedLeft = 0;
-    if (targetSpeedRight > 255)
-        targetSpeedRight = 255;
-    else if (targetSpeedRight < 0)
-        targetSpeedRight = 0;
-    Serial.print(" ");
-    Serial.print(targetSpeedLeft);
-    Serial.print(" ");
-    Serial.print(targetSpeedRight);
-    Serial.print(" ");
-
-    motorFL.setPID(targetSpeed+targetSpeedLeft, 1, 0.005, 0.006); //SEGUIR AJUSTANDO
-    motorFR.setPID(targetSpeed+targetSpeedRight, 1, 0.005, 0.006);
-    motorBL.setPID(targetSpeed+targetSpeedLeft, 1, 0.005, 0.006);
-    motorBR.setPID(targetSpeed+targetSpeedRight, 1, 0.005, 0.006);
-
-    Serial.print(motorFL.getRPM());
-    Serial.print(" ");
-    Serial.print(motorFR.getRPM());
-    Serial.print(" ");
-    Serial.print(motorBL.getRPM());
-    Serial.print(" ");
-    Serial.println(motorBR.getRPM());
-}
-
-Motor Movement::getMotorFL() {
-    return motorFL;
-}
-
-Motor Movement::getMotorFR() {
-    return motorFR;
-}
-
-Motor Movement::getMotorBL() {
-    return motorBL;
-}
-
-Motor Movement::getMotorBR() {
-    return motorBR;
-}
-
 float Movement::getRPMFL() {
     return motorFL.getRPM();
 }
@@ -148,31 +69,30 @@ float Movement::getRPMBR() {
 }
 
 void Movement::stopMotors() {
-    
-    for(int i=0; i<4; i++){
+    for(int i=0; i<4; ++i){
         motor[i].motorStop();
     }
 }
 
-void Movement:: forwardMotors(uint8_t pwms[4]) {
+void Movement:: forwardMotors(const uint8_t pwms[4]) {
     for(int i=0; i<4; i++){
         motor[i].motorForward(pwms[i]);
     }
 }
 
-void Movement:: backwardMotors(uint8_t pwms[4]) {
+void Movement:: backwardMotors(const uint8_t pwms[4]) {
     for(int i=0; i<4; i++){
         motor[i].motorBackward(pwms[i]);
     }
 }
-void Movement:: turnRight(uint8_t pwms[4]) {
+void Movement:: turnRight(const uint8_t pwms[4]) {
     motor[0].motorBackward(pwms[0]);
     motor[1].motorBackward(pwms[1]);
     motor[2].motorBackward(pwms[2]);
     motor[3].motorBackward(pwms[3]);
 }
 
-void Movement:: turnLeft(uint8_t pwms[4]) {
+void Movement:: turnLeft(const uint8_t pwms[4]) {
     motor[0].motorForward(pwms[0]);
     motor[1].motorForward(pwms[1]);
     motor[2].motorForward(pwms[2]);
@@ -195,10 +115,6 @@ void Movement:: moveMotors(MotorState state) {
         }
         
         case (MotorState::Forward): {
-            // 1- SET angulo deseado
-            // 2- Llamar a la funcion computeStraight
-            // 3- meter al arreglo
-            // 4- llamar a forward motors
             pidStraight.computeStraight(targetOrientation,currentOrientation, pwmLeft, pwmRight);
 
             pwms[static_cast<int>(MotorID::FRONT_LEFT)]= pwmLeft;
@@ -250,7 +166,6 @@ void Movement::updateTics(MotorID motorId) {
     } */
 } 
 
-
 int Movement::getBackLeftEncoderTics() {
     return motor[static_cast<int>(MotorID::BACK_LEFT)].getEncoderTics();
 }
@@ -266,73 +181,3 @@ int Movement::getBackRightEncoderTics() {
 int Movement::getFrontRightEncoderTics() {
     return motor[static_cast<int>(MotorID::FRONT_RIGHT)].getEncoderTics();
 }
-
-// no comentar
-/* void Movement:: setSpeed(double targetSpeed){
-    // El targetSpeed se multiplica por 1000 porque es una conversion de unidades de m/s a mm/s
-    if (targetSpeed == 0) {
-        stopMotors();
-        return;
-    }
-    // TODO: checar como calcular el pwm 
-    // compute(targetSpeed, currentSpeed, pwm, pidTics, 500, 1, false);
-
-
-
-    switch (speedSign)
-    {
-        case (0):{
-            stopMotors();
-            break;
-        }
-        case (1):{
-            forwardMotors();
-            break;
-        }
-        case (-1):{
-            backwardMotors();
-            break;
-        }
-    }
-} */
-
-
-//no comentar
-/* void Movement:: motorSpeedPID(double targetSpeed, bool debug){
-    int speedSign = min(1, max(-1, targetSpeed * 1000));
-    this->targetSpeed = fabs(targetSpeed);
-    double tmpPwm = Motor::pwm;
-    switch (speedSign)
-    {
-        case (0):{
-            stopMotors();
-            break;
-        }
-        case (1):{
-            forwardMotors();
-            break;
-        }
-        case (-1):{
-            backwardMotors();
-            break;
-        }
-    }    
-} */
-
-// TODO: comprobar que estos dos se implementen en el PID
-/* 
-void Movement::setSpeed(MotorID motorId, double targetSpeed){
-    int index = static_cast<int>(motorId);
-    if (index >= 0 && index < 4){
-        motor[index].setSpeed(targetSpeed);
-    }
-}
-*/
-
-void Movement::setMotorSpeed(int leftSpeed, int rightSpeed) {
-    motor[static_cast<int>(MotorID::FRONT_LEFT)].setPWM(leftSpeed);
-    motor[static_cast<int>(MotorID::FRONT_RIGHT)].setPWM(rightSpeed);
-    motor[static_cast<int>(MotorID::BACK_LEFT)].setPWM(leftSpeed);
-    motor[static_cast<int>(MotorID::BACK_RIGHT)].setPWM(rightSpeed);
-
-} 
