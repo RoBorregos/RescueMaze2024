ef2c60cbe293cb9dd062eaf9826a61a062d4393e
GerardoFJ <A00838645@tec.mx>
2024-03-19 05:10:51 -0600

    Prueba vision

---
+++ jetson_vision/take_picture.py
@@ -0,0 +1,56 @@
+
+
+import cv2
+
+def gstreamer_pipeline(
+    sensor_id=0,
+    capture_width=1920,
+    capture_height=1080,
+    display_width=960,
+    display_height=540,
+    framerate=30,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            sensor_id,
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+
+def show_camera():
+    window_title = "CSI Camera"
+    print(gstreamer_pipeline(flip_method=0))
+    video_capture = cv2.VideoCapture(gstreamer_pipeline(flip_method=0), cv2.CAP_GSTREAMER)
+    if video_capture.isOpened():
+        try:
+            ret, frame = video_capture.read()
+            if not ret:
+                print("Error: Unable to capture frame.")
+                video_capture.release()  # Release the camera device
+                exit()
+            cv2.imshow("Frame", frame)
+            cv2.waitKey(0) 
+            cv2.imwrite("./pictures/pic.jpg", frame)
+
+        finally:
+            video_capture.release()
+            cv2.destroyAllWindows()
+    else:
+        print("Error: Unable to open camera")
+
+
+if __name__ == "__main__":
+    show_camera()
