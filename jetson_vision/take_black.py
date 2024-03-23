b24fa7992358bb4b036474119ec9a6b93eb081bc
GerardoFJ <A00838645@tec.mx>
2024-03-23 00:19:48 -0600

    dataset created

---
+++ jetson_vision/take_black.py
@@ -0,0 +1,118 @@
+import cv2
+import numpy as np
+import camera_activate
+import time as t 
+from PIL import Image
+
+
+def areaFilter(minArea, inputImage):
+    # Perform an area filter on the binary blobs:
+    componentsNumber, labeledImage, componentStats, componentCentroids = \
+        cv2.connectedComponentsWithStats(inputImage, connectivity=4)
+    # Get the indices/labels of the remaining components based on the area stat
+    # (skip the background component at index 0)
+    remainingComponentLabels = [i for i in range(1, componentsNumber) if componentStats[i][4] >= minArea]
+    # Filter the labeled pixels based on the remaining labels,
+    # assign pixel intensity to 255 (uint8) for the remaining pixels
+    filteredImage = np.where(np.isin(labeledImage, remainingComponentLabels) == True, 255, 0).astype('uint8')
+
+    return filteredImage
+
+
+def rotate_image(binary_img):
+    coords = np.column_stack(np.where(binary_img > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = binary_img.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(binary_img, M, (w, h), flags=cv2.INTER_CUBIC)
+    return rotated
+
+def process_image(img):
+    imgFloat = img.astype(np.float) / 255.0
+    kChannel = 1 - np.max(imgFloat, axis=2)
+    kChannel = (255*kChannel).astype(np.uint8)
+    #cv2.imshow('kChannel', kChannel)
+    binaryThresh = 170
+    _, binaryImage = cv2.threshold(kChannel, binaryThresh, 255, cv2.THRESH_BINARY)
+    #cv2.imshow("binary", binaryImage)
+    kernelSize = 3
+    opIterations = 2
+    morphKernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernelSize, kernelSize))
+    binaryImage = cv2.morphologyEx(binaryImage, cv2.MORPH_CLOSE, morphKernel, None, None, opIterations, cv2.BORDER_REFLECT101)
+    #cv2.imshow("binary2", binaryImage)
+    #dudoso
+    minArea = 1000
+    filteredImage = areaFilter(minArea, binaryImage)
+    #cv2.imshow("filtered", filteredImage)
+    return filteredImage
+
+
+    
+camera_source = camera_activate.gstreamer_pipeline(flip_method=0)
+
+
+video_capture = cv2.VideoCapture(camera_source, cv2.CAP_GSTREAMER)
+#video_capture = cv2.VideoCapture(camera_source)
+
+def generate_bbox(img):
+    mask_ = Image.fromarray(img)
+    bbox = mask_.getbbox()
+    if bbox is not None :
+        x1,y1,x2,y2 = bbox
+        frame = img[y1:y2,x1:x2]
+    else:
+        frame =""
+    
+    return frame
+
+def post_processing(img,dilate):
+    size_img = cv2.resize(img,(100,100))
+    size_img = cv2.dilate(size_img, None, iterations=dilate)
+    size_img = cv2.resize(img,(32,32))
+    return size_img
+    
+
+if video_capture.isOpened():
+    try:
+        count = 0
+        while True:
+            ret_val, img = video_capture.read()
+            binary_img = process_image(img)
+            #cv2.imshow("binary", binary_img)      
+            new_img = rotate_image(binary_img)
+            #cv2.imshow("Rotated", new_img)
+            new_img = generate_bbox(new_img)
+            if new_img != "":
+                
+                new_img = post_processing(new_img,4)
+                cv2.imshow("Final", new_img)
+
+
+            keyCode = cv2.waitKey(10) & 0xFF
+            # Stop the program on the ESC key or 'q'
+            if keyCode == ord('p'):
+                if new_img != "":
+                    cv2.imwrite(f"./dataset_two/s/{str(count)}.jpg",new_img)
+                    count+=1
+                    print(f"image {str(count)} taked")
+            if keyCode == 27 or keyCode == ord('q'):
+                break
+            
+                
+
+    finally:
+            video_capture.release()
+            cv2.destroyAllWindows()
+else:
+    print("Error: Unable to open camera")
+
+
+#89 131 106 223 0 255   azul
+    #89 131 106 223 0 255 amarillo
+
+
