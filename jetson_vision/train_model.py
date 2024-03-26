64f33a432e38286299952e1614ab1ac455955f5f
afr2903 <afr102903@gmail.com>
2024-03-26 00:08:33 -0600

    Added shit

--- jetson_vision/train_model.py
+++ jetson_vision/train_model.py
@@ -129,7 +129,31 @@ imshow(out, title=[class_names[x] for x in classes])
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
+def visualize_model(model, num_images=6):
+    was_training = model.training
+    model.eval()
+    images_so_far = 0
+    fig = plt.figure()
 
+    with torch.no_grad():
+        for i, (inputs, labels) in enumerate(dataloaders['val']):
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+
+            for j in range(inputs.size()[0]):
+                images_so_far += 1
+                ax = plt.subplot(num_images//2, 2, images_so_far)
+                ax.axis('off')
+                ax.set_title(f'predicted: {class_names[preds[j]]}')
+                imshow(inputs.cpu().data[j])
+
+                if images_so_far == num_images:
+                    model.train(mode=was_training)
+                    return
+        model.train(mode=was_training)
 
 
 model_ft = models.resnet18(weights='IMAGENET1K_V1')
@@ -150,3 +174,7 @@ exp_lr_scheduler = lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
 
 model_ft = train_model(model_ft, criterion, optimizer_ft, exp_lr_scheduler,
                        num_epochs=25)
+#save the model
+torch.save(model_ft.state_dict(), 'model.pth')
+
+visualize_model(model_ft)
